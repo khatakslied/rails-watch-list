635fae57966f5819916be4e5186382b4666cfd30
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-11-12 00:43:46 +0900

    Styling added.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,7 +8,7 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    if @bookmark.save!
+    if @bookmark.save
       redirect_to list_path(@list)
     else
       render :new
