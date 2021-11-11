72d2fb68d85293c10dee9ea2ead28c9dd6030ce3
Ryan Johnson <ryan.a.y.johnson@gmail.com>
2021-11-11 16:12:32 +0900

    Lists controller done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+url = 'https://api.themoviedb.org/3/movie/top_rated?api_key=987db1965f59b1afa8d4f0943b6b6584'
+movies = JSON.parse(URI.open(url).read)
+
+puts "Clearing database..."
+Movie.destroy_all
+
+puts "Adding movies to database..."
+movies["results"].each do |movie|
+  Movie.create!(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}",
+    rating: movie["vote_average"]
+  )
+end
